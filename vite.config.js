054209addc5a2e73476b6de17bd0a0a7dc269578
Hans Daduya <hansdaduya@yahoo.com>
2023-01-06 22:04:13 +0800

    configure vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				help: resolve(__dirname, "help.html"),
+			},
+		},
+	},
+});
